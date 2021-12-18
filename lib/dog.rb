cba8feb35f6383ea60202fdac98112911dd8c3f1
Nicholas Shephard <shephardn@gmail.com>
2021-12-17 19:41:20 -0600

    init

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,52 @@
+require "pry"
+
 class Dog
 
+    attr_accessor :name, :breed, :id
+
+    def initialize(name:, breed:, id: nil)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY
+                name TEXT,
+                breed TEXT
+            )
+            SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+
+    end
+
+    def self.save
+
+    end
+
+    def self.create
+        self.save
+    end
+
+    def self.all
+
+    end
+
+    def self.find_by_name(name)
+
+    end
+
+    def self.find(id)
+
+    end
+
+
+
 end
+
+binding.pry
