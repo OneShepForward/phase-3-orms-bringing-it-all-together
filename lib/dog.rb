6b3c21738aae62482b44acbc8bdd37d8b846de9a
Nicholas Shephard <shephardn@gmail.com>
2021-12-17 20:15:27 -0600

    1

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,15 +29,18 @@ class Dog
     end
 
     def self.save
-        sql = <<-SQL
-        INSERT INTO dogs (name, breed)
-        VALUES (?, ?)
-        SQL
+        if self.id
+            self.update
+        else
+            sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+            SQL
 
-        DB[:conn].execute(sql, self.name, self.breed)
+            DB[:conn].execute(sql, self.name, self.breed)
 
-        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-
+            self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
         self
     end
 
@@ -57,7 +60,8 @@ class Dog
 
     # end
 
-
+    def self.update
+    end
 
 end
 
